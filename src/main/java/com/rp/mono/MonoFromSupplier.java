0eca9f74964959d872ee0e1cbc2fe721eacc4e83
masaki nakamura <gmk4n0n@gmail.com>
2023-02-18 08:57:44 +0900

    subscribing mono with data just created (using fromSupplier)

---
+++ src/main/java/com/rp/mono/MonoFromSupplier.java
@@ -0,0 +1,23 @@
+package com.rp.mono;
+
+import com.rp.util.Util;
+import reactor.core.publisher.Mono;
+
+public class MonoFromSupplier {
+    public static void main(String[] args) {
+
+        // use "just" only when you have data already
+        // Mono<String> mono = Mono.just(getName());
+
+        // use fromSupplier when you are going to calculate new data.
+        Mono<String> mono = Mono.fromSupplier(() -> getName());
+        mono.subscribe(
+                Util.onNext()
+        );
+    }
+
+    private static String getName() {
+        System.out.println("Generating name");
+        return Util.faker().name().fullName();
+    }
+}
