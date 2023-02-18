7fc2ac79a529e72aab615931f6c491570fd66e1d
masaki nakamura <gmk4n0n@gmail.com>
2023-02-18 13:35:51 +0900

    adding a non-blocking behaviour to Lec06SupplierRefactoring class

--- src/main/java/com/rp/mono/Lec06SupplierRefactoring.java
+++ src/main/java/com/rp/mono/Lec06SupplierRefactoring.java
@@ -2,11 +2,26 @@ package com.rp.mono;
 
 import com.rp.util.Util;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 public class Lec06SupplierRefactoring {
     public static void main(String[] args) {
         //getName(); // this won't execute the pipeline, this will only create the pipeline
-        getName().subscribe(Util.onNext());
+
+        // This still has a blocking behavior
+        //getName().subscribe(Util.onNext());
+
+        // Asynchronous behaviour (non-blocking)
+        // pipeline is being executed but is not being printed.
+        // the main thread is being exited before the pipeline gets executed completely
+        // to see the result we can add a delay (we don't do it in real life)
+        getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(Util.onNext());
+
+        Util.sleepSeconds(4);
+
     }
 
     private static Mono<String> getName() {
