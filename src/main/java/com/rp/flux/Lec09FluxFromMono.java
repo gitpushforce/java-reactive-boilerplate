45117c1e7736c94af2399271c5fc77999207a4c3
masaki nakamura <gmk4n0n@gmail.com>
2023-02-21 04:01:32 +0900

    create flux from a mono

---
+++ src/main/java/com/rp/flux/Lec09FluxFromMono.java
@@ -0,0 +1,20 @@
+package com.rp.flux;
+
+import com.rp.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Lec09FluxFromMono {
+
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("a");
+
+        Flux<String> flux = Flux.from(mono);
+        flux.subscribe(Util.onNext());
+
+    }
+
+    private static void doSomething (Flux<String> flux) {
+
+    }
+}
